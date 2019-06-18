228d3ac54dc8f85a7f51db604e423096f2ec16f2
Victoria Chen <Victoria Chen>
2019-06-18 13:11:44 +0200

    react setup and fetch test data from openweathermap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { async } from 'q';
+
 
 function App() {
+
+  const url = 'https://api.openweathermap.org/data/2.5/forecast?id=1851632';
+  const url2 = 'https://api.openweathermap.org/data/2.5/weather?id=2172797';
+  const apiKey = '21e9711869d651b73195343d41b52a78';
+  const [forecast, setForecast] = useState(null);
+  
+  // userEffect called after page re-renders, putting [] limits to first time, like component did mount
+  useEffect( async () => {
+    console.log( 'useEffect' );
+    const response = await fetch( url + '&appid=' + apiKey );
+    const data = await response.json();
+    console.log( data );  
+    const city = data.city;
+    console.log( 'city', city );
+    const reports = data.list;
+    console.log( 'reports ', reports );
+
+    setForecast( { city: city, reports: reports } );
+  }, [] );
+
   return (
     <div className="App">
-      <header className="App-header">
+
+
+
+      { forecast && <div> {forecast.city.name} </div>}
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +46,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
     </div>
   );
 }
