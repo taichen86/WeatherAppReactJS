8fb5a321c11bc94668fc92f972ee7085aa535d1e
Victoria Chen <Victoria Chen>
2019-06-19 10:09:53 +0200

    dynamic bg images by city name

--- src/App.js
+++ src/App.js
@@ -12,22 +12,30 @@ function App() {
   const url = 'https://api.openweathermap.org/data/2.5/forecast?';
   const apiKey = '21e9711869d651b73195343d41b52a78';
   const [forecast, setForecast] = useState(null);
-  const [cityID, setCityID] = useState('6359304');  // initialize to Madrid
+  const [cityID, setCityID] = useState('6359304');  // initialize location to Madrid
   
+  const [cityImages, setCityImages] = useState(null); // list of all available cities with images for background
+  const [bgImageCity, setBGImageCity] = useState('Madrid'); // initialize location to Madrid
+  const [bgImageURL, setBGImageURL] = useState('city-madrid.jpg');
 
-  // this callback runs once after first render
+  // this callback runs only once after first render, similar to componentDidMount
   useEffect( () => {
-    // TODO: find current location
-    // console.log( '=== process cities list ===');
-    // const city = allcities.find( city => city.name == 'Madrid' );
-    // console.log( city );
+    // TODO: find current location?
+    async function fetchMyAPI( ) {
+      console.log( 'initial useEffect, get cities list')
+      const path = 'https://api.teleport.org/api/urban_areas/';
+      console.log( path );
+      const response = await fetch( path );
+      const data = await response.json();
+      setCityImages( data._links["ua:item"] );
+    }
+    fetchMyAPI();
   }, []);
 
   // this callback runs 
   useEffect( () => {
     console.log( 'fetch data effect' );
     async function fetchMyAPI( ) {
-      let config = {};
       const path = url + 'id=' + cityID + '&appid=' + apiKey;
       console.log( path );
       const response = await fetch( path );
@@ -38,20 +46,46 @@ function App() {
     fetchMyAPI();
   }, [cityID] );
 
+
+  useEffect( () => {
+    console.log( '=== UPDATE IMAGE BG ===' );
+    async function fetchMyAPI( ) {
+      const path = 'https://api.teleport.org/api/urban_areas/slug:' + bgImageCity + '/images/';
+      console.log( bgImageCity );
+      const response = await fetch( path );
+      const data = await response.json();
+      console.log( 'BG IMG', data );
+      console.log( data.photos[0].image.web );
+      // change bg image url link
+      if( data.photos[0].image.web != undefined ){
+        setBGImageURL( data.photos[0].image.web );
+      }
+
+    }  
+    fetchMyAPI();
+  }, [bgImageCity] );
+
+
+
   function getForecastFor( cityname ){
     console.log( 'getForecastFor... ', cityname );
-    const match = allcities.find( item => item.name == cityname );
-    if( match != undefined ){
-      console.log( 'match found ', match );
-      setCityID( match.id );
+    const weathermatch = allcities.find( item => item.name.toLowerCase() == cityname.toLowerCase() ); // TODO: insert autocomplete
+    if( weathermatch != undefined ){
+      console.log( 'match found ', weathermatch );
+      setCityID( weathermatch.id );
+
+      // update bg image
+      const imagematch = cityImages.find( item => item.name.toLowerCase() == weathermatch.name.toLowerCase() );
+      console.log( 'CITY IMAGES MATCH ', imagematch.name );
+      setBGImageCity( imagematch.name.replace(' ', '-').toLowerCase() );
+
     }
   }
 
 
-
   const styles = {
     blurred: {
-      backgroundImage: "url(city-madrid.jpg)",
+      backgroundImage: 'url(' + bgImageURL + ')',
       filter: "blur(12px)",
       height: "500px",
       backgroundPosition: "center",
@@ -66,10 +100,9 @@ function App() {
 
       <div style={styles.outterPanel}></div>
 
-
       {/* { cityID && <div> {cityID} </div>} */}
 
-      { forecast && <WeatherPanel data={forecast}></WeatherPanel> }
+      { forecast && <WeatherPanel data={forecast} bgURL={bgImageURL}></WeatherPanel> }
       <SearchBox search={getForecastFor}></SearchBox>
 
 
--- src/components/WeatherPanel.js
+++ src/components/WeatherPanel.js
@@ -12,7 +12,7 @@ function WeatherPanel( props ){
     // group data by day - show highest temperature 
     const days = [];
     props.data.reports.forEach( item => {
-        console.log( 'doing ... ', item );
+        // console.log( 'doing ... ', item );
          const date = item.dt_txt.slice(0,10);
         // console.log( date );
             const samedate = days.find( day => day.dt_txt.includes( date ) );
@@ -22,9 +22,9 @@ function WeatherPanel( props ){
         }else{
             // console.log( 'same day found: ', sameday );
             // replace if temperature higher
-            console.log( 'existing ', samedate.main.temp );
+            // console.log( 'existing ', samedate.main.temp );
             if( item.main.temp > samedate.main.temp ){
-                console.log( 'replace with', item.main.temp );
+                // console.log( 'replace with', item.main.temp );
                 days.pop();
                 days.push( item )
             }
@@ -45,7 +45,9 @@ function WeatherPanel( props ){
 
     const styles = {
         panel: {
-            backgroundImage: "url(city-madrid.jpg)",
+            backgroundImage: 'url(' + props.bgURL + ')',
+            backgroundPosition: "center",
+            // backgroundSize: "cover",
         },
         innerPanel: {
             backgroundColor: 'rgba(50, 50, 50, 0.5)',
@@ -60,8 +62,6 @@ function WeatherPanel( props ){
 
     }
 
-
-
     return(
 
         <div style={styles.outterPanel}>
@@ -72,7 +72,7 @@ function WeatherPanel( props ){
                     <h3>{props.data.city.name}</h3>
                     <h6> current time </h6>
                     <div className="current-panel">
-                        <div className="item temp">+<span className="text-large">{currentTemp}</span>&deg;</div>
+                        <div className="item temp"><span>+</span><span className="text-large">{currentTemp}</span><span>&deg;</span></div>
                         <div className="item icon">
                             <img src={currentIconURL}></img>
                             <div className="main">{currentWeather.weather[0].main}, {currentWeather.weather[0].description}</div>
