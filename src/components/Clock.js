3668c000069b7d41288952d5f678f33b9d9122f0
Victoria Chen <Victoria Chen>
2019-06-19 15:45:20 +0200

    add clock component

---
+++ src/components/Clock.js
@@ -0,0 +1,54 @@
+import React, {useState, useEffect} from 'react';
+
+function Clock(){
+
+    const options = { weekday: 'long', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false  };
+
+    const [time, setTime] = useState( new Date() );
+    function tick(){
+        setTime( new Date() );
+    }
+
+    setInterval( tick, 1000);
+    return(
+        <div>{time.toLocaleDateString( "en-US", options )}</div>
+    );
+}
+
+// class Clock extends React.Component {
+
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//           date: new Date(),
+//           timerID: null
+//         };
+//     }
+
+//     componentDidMount() {
+//         this.timerID = setInterval(
+//           () => this.tick(),
+//           1000
+//         );
+//       }
+
+//     componentWillUnmount() {
+//         clearInterval(this.timerID);
+//     }
+
+//     tick() {
+//     this.setState({
+//         date: new Date()
+//     });
+//     }
+  
+//     render() {
+//       return (
+//         <div>
+//           {this.state.date.toLocaleTimeString()}
+//         </div>
+//       );
+//     }
+//   }
+
+export default Clock;
--- src/components/WeatherPanel.js
+++ src/components/WeatherPanel.js
@@ -1,34 +1,29 @@
 import React, {useState, useEffect} from 'react';
-
-import '../style/WeatherPanel.css';
-
 import DayForecastCard from './DayForecastCard';
-import { isProperty } from '@babel/types';
+import '../style/WeatherPanel.css';
+import Clock from './Clock';
+
 
 
 function WeatherPanel( props ){
 
-    // const [isFading, setIsFading] = useState(true); // fade in panel upon render
-    useEffect( () => { 
+    useEffect( () => {  // TODO: change to setFadeIn
         console.log( 'RERENDER - fade in bg image' );
         document.getElementById( "outterPanel" ).classList = [];
     });
 
     console.log( 'weather panel props: ', props );
 
-    // group data by date - show highest temperature 
+    // group data by date - keep highest temperature 
     const days = [];
     props.data.reports.forEach( item => {
-        // console.log( 'doing ... ', item );
-         const date = item.dt_txt.slice(0,10);
-        // console.log( date );
+        const date = item.dt_txt.slice(0,10);
         const samedate = days.find( day => day.dt_txt.includes( date ) );
-        // new day
+        
         if( samedate == undefined ){
             days.push( item );
         }else{
             if( item.main.temp > samedate.main.temp ){
-                // console.log( 'replace with', item.main.temp );
                 days.pop();
                 days.push( item )
             }
@@ -37,27 +32,32 @@ function WeatherPanel( props ){
     // console.log( days );
 
     const daysPanel = days.map( item => {
-        // console.log( item );
         return <DayForecastCard data={item}></DayForecastCard>
     });
     // console.log( daysPanel );
 
+
     const currentWeather = props.data.reports[0];
     console.log( 'current weather ', currentWeather );
-    const currentDate = new Date( currentWeather.dt_txt ).getDay() + ', ' + currentWeather.dt_txt;
+    const dayNum = new Date( currentWeather.dt_txt ).getDay();
+    const daysText = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDate = daysText[dayNum] + ', ' + currentWeather.dt_txt.slice( 0, 10 );
     const currentTemp = (currentWeather.main.temp - 273.15).toFixed(0);
     const currentIconURL = 'http://openweathermap.org/img/w/' + currentWeather.weather[0].icon + '.png';
+
+    const [currentTime, setCurrentTime] = useState(new Date());
+
+    function timer(){
+        // setCurrentTime( new Date() );
+    }
+    setInterval( timer, 1000);
 
     const styles = {
         panel: {
             backgroundImage: 'url(' + props.bgURL + ')',
-            backgroundPosition: "center"
-           
-        },
-        innerPanel: { // TODO: move this to stylesheet
-            backgroundColor: 'rgba(50, 50, 50, 0.5)',
+            backgroundPosition: "center",
+            backgroundSize: "cover"
         }
-  
 
     }
 
@@ -65,18 +65,22 @@ function WeatherPanel( props ){
 
         <div id="outterPanel" className="hidden">
             <div style={styles.panel}>
-                <div style={styles.innerPanel}>
+                <div id="innerPanel">
 
                     <div className="title">
                         <h3>{props.data.city.name}</h3>
-                        <h6> 
-
-                        </h6>
+                        <div id="current-time">
+                            <Clock></Clock>
+                        </div>
                     </div>
-                    <div className="current-panel">
-                        <div className="item temp"><span>+</span><span className="text-large">{currentTemp}</span><span>&deg;</span></div>
-                        <div className="item icon">
-                            <img src={currentIconURL}></img>
+                    <div id="current-panel">
+                        <div className="item temp">
+                            <span id="plus-sign">+</span>
+                            <span id="main-temp">{currentTemp}</span>
+                            <span>&deg;</span>
+                        </div>
+                        <div className="item">
+                            <img id="main-icon" src={currentIconURL}></img>
                             <div className="main">{currentWeather.weather[0].main}, {currentWeather.weather[0].description}</div>
                         </div>
                         <div className="item">
